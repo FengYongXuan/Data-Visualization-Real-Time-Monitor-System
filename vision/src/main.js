52c56955b2bdcfcbb9991118a441e42e3118f9d4
xuan <1906574057@qq.com>
2021-10-02 11:22:47 +0800

    '完成了商家销售统计柱状图表的设计'

--- vision/src/main.js
+++ vision/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+// 引入全局的样式文件
+import './assets/css/global.less'
 
 // 请求基准路径的配置
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
---
+++ vision/src/views/SellerPage.vue
@@ -0,0 +1,21 @@
+<!--
+针对 /sellerpage这条路径显示出来的
+在该组件中，通过子组件注册的方式，要显示出Seller.vue这个组件
+-->
+<template>
+  <div class="com-page">
+    <seller></seller>
+  </div>
+</template>
+
+<script>
+import Seller from '@/components/Seller'
+export default {
+  components: {
+    seller: Seller
+  }
+}
+</script>
+
+<style>
+</style>
