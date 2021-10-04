621b5a420eab07800274c228a2d5355b2655a8d8
xuan <1906574057@qq.com>
2021-10-04 14:02:28 +0800

    '完成了销量趋势图表（折线图）'

--- vision/src/main.js
+++ vision/src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import axios from 'axios'
 // 引入全局的样式文件
 import './assets/css/global.less'
+// 引入字体的文件
+import './assets/font/iconfont.css'
 
 // 请求基准路径的配置
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
---
+++ vision/src/views/TrendPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="com-page">
+    <trend></trend>
+  </div>
+</template>
+
+<script>
+import Trend from '../components/Trend.vue'
+export default {
+  components: {
+    trend: Trend
+  }
+}
+</script>
+
+<style>
+</style>
