fb4bcf5f70393cbc7a1380939722bc45430cdb70
xuan <1906574057@qq.com>
2021-10-07 13:19:48 +0800

    '完成了库存销量图表（环形饼图）'

---
+++ vision/src/views/StockPage.vue
@@ -0,0 +1,21 @@
+<!--
+针对/stockpage这条路径而显示出来
+在这个组件中，通过注册子组件的方式显示Stock.vue这个组件
+-->
+<template>
+  <div class="com-page">
+    <stock></stock>
+  </div>
+</template>
+
+<script>
+import Stock from '../components/Stock.vue'
+export default {
+  components: {
+    stock: Stock
+  }
+}
+</script>
+
+<style>
+</style>
